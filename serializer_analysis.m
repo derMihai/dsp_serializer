167116d7d0dde7d665a52d68c764e5b3d0f0524d
Mihai Renea <mihai.renea@fu-berlin.de>
2025-07-24 18:15:20 +0200

    serializer: tested: TODO: fix output register order

--- serializer_analysis.m
+++ serializer_analysis.m
@@ -1,7 +1,15 @@
 clear signals;
+
+% data_out_n_sim = zeroes(length(sym_rx));
+% data_out_sim = zeroes(length(sym_rx));
+% 
+% for i = 1:1:length(sym_rx) - (8 * N_dft) - 1
+%     data_out_n_sim = 
+% end
  
 signals = {
    'data_in', 'single';
+   'bit_begin', 'single';
    'data_en', 'single';
    'out.data_en_pulse', 'single';
    'out.data_out_n', 'bus_value_h';
--- serializer_init.m
+++ serializer_init.m
@@ -1,4 +1,31 @@
-data_in = [0 1 1 1 0 0 0 0 1 1 1 0 0 0 0 0 1 1 1 0 0 0 1 0 0 1 0 0 1 0 0 1 1 0 0 0 ];
-data_en = [0 1 0 0 1 1 0 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 1 1 1 0 1 1];
+N_dft = 10;
+
+%data_in = [0 1 1 1 0 0 0 0 1 1 1 0 0 0 0 0 1 1 1 0 0 0 1 0 0 1 0 0 1 0 0 1 1 0 0 0 ];
+%data_en = [0 1 0 0 1 1 0 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 1 1 1 0 1 1];
 %data_en = zeros(length(data_in), 1) + 1;
-N_sim = size(data_in);
+
+tx_bytes = [bin2dec('0100 0111'), 1, 2, 3, 12, 13, 14];         % Synchronsequenz und zu übertragende Datenbytes
+
+tx_bit_array = dec2bin(tx_bytes, 8).';              % Umwandlung der Bytes in Binärzahlen '0' und '1'
+tx_bit_array = double(tx_bit_array == '1');         % Umwandlung der Binärwerte von char zu double
+tx_bit_array_lsb_first = flipud(tx_bit_array);
+tx_bit_stream = tx_bit_array_lsb_first(:); 
+
+syms = tx_bit_stream;
+
+man_overs = zeros(length(syms) * N_dft, 1);
+bit_begin = zeros(length(man_overs), 1);
+bit_begin(1:N_dft:end) = 1;
+bit_begin(2:N_dft*8:end) = 1;
+
+for i = 1:1:length(syms)
+    for j = 1:1:N_dft
+        man_overs((i - 1) * N_dft + j) = syms(i);
+    end
+end
+
+data_in = man_overs;
+data_en = zeros(length(data_in), 1) + 1;
+
+N_sim = length(data_in);
+
