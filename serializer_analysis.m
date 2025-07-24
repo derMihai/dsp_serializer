78c6d50d181ed0b4f7337f4a5eb46128feb112e6
Mihai Renea <mihai.renea@fu-berlin.de>
2025-07-24 14:20:25 +0200

    serializer: using shift tap register

--- serializer_analysis.m
+++ serializer_analysis.m
@@ -6,9 +6,8 @@ signals = {
    'out.data_en_pulse', 'single';
    'out.data_out_n', 'bus_value_h';
    'out.data_out', 'bus_value_h';
-   'out.shift_reg', 'bus_value_h';
    'out.data_out_valid', 'single';
-    };
+};
 
 DispTime=0;
 LaPlot(signals,[DispTime,DispTime+50,1]);
