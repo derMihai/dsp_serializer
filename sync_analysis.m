bcb7df315086432292fcbce5e06a77a891206727
Mihai Renea <mihai.renea@fu-berlin.de>
2025-07-31 17:10:49 +0200

    deser: fixed wrong spacing. matcher: TODO: merge sync and matching_end states

--- sync_analysis.m
+++ sync_analysis.m
@@ -11,12 +11,18 @@ signals = {
 %     'out.set_MATCHING_END', 'single';
 %     'out.set_SYNC', 'single';
 
+    'data_in', 'single';
+    'matcher_reset', 'single';
+    'data_en', 'single';
     'out.NOT_MATCHING', 'single';
     'out.MATCHING', 'single';
     'out.MATCHING_END', 'single';
     'out.SYNC', 'single';
     'out.MATCH_LEN', 'bus_value_d';
-
+    'out.data', 'bus_value_h';
+    'out.data_n', 'bus_value_h';
+    'out.match', 'single';
+    'out.valid', 'single';
 };
 
 DispTime=0;
--- sync_init.m
+++ sync_init.m
@@ -11,14 +11,23 @@ DataClock(1:2:end) = 1;
 Manchester_bits = xor(tx_bit_stream_2, DataClock);  % Manchester Codierung
 
 % Symbol Mapping: Bitwert 0 => Symbolwert 1, Bitwert 1 => Symbolwert -1
-Manchester_symbols = 1 * double(Manchester_bits == 0) + (-1) * double(Manchester_bits == 1);
+% Manchester_symbols = 1 * double(Manchester_bits == 0) + (-1) * double(Manchester_bits == 1);
 
 N_clk_per_symbol = 10;                              % Anzahl der Takte pro Symbol
 
-Manch_symb_oversamp = kron(Manchester_symbols, [1; zeros(N_clk_per_symbol-1, 1)]); % Überabtastung der Manchester-encodierten Datensymbole
+% Manch_symb_oversamp = kron(Manchester_bits, [1; zeros(N_clk_per_symbol-1, 1)]); % Überabtastung der Manchester-encodierten Datensymbole
+man_overs = zeros(length(Manchester_bits) * N_dft, 1);
+% bit_begin = zeros(length(man_overs), 1);
+% bit_begin(1:N_dft:end) = 1;
+% bit_begin(2:N_dft*8:end) = 1;
 
-data_in = Manch_symb_oversamp;
+for i = 1:1:length(Manchester_bits)
+    for j = 1:1:N_dft
+        man_overs((i - 1) * N_dft + j) = Manchester_bits(i);
+    end
+end
+data_in = man_overs;
 data_en = zeros(length(data_in), 1) + 1;
-matcher_reset = [ 1 0 ];
+matcher_reset = [ 1 1 0 ];
 
 N_sim = length(data_in);
